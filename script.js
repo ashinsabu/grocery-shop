645ff917da372e818bbc01ee474cd19ab0c69d5f
ashinsabu <ashin.sabu3@gmail.com>
2022-07-06 16:42:12 +0530

    Added a page to create your own custom atta

--- script.js
+++ script.js
@@ -231,25 +231,224 @@ categoryContainers.forEach((categoryContainer) => {
 const categSquares = document.querySelectorAll('.atta');
 
 categSquares.forEach((categSquare) => {
-    categSquare.addEventListener('click',() => {
-        const billsec = document.querySelector('.billing-section');
-        billsec.style.display = 'none';
-        topRow.style.display = 'flex';
-        displayItems(categSquare.dataset.categ);
-        shoppingarea.style.display = 'grid';
-        const products = document.querySelector('#products');
-        products.style.display = 'none';
-        // console.log(categoryContainers);
-        categoryContainers.forEach((x) => {
-            x.classList.remove('active');
-            // console.log(x.childNodes[1].dataset.categoryname);
-            // console.log(categSquare.dataset.categ);
-            if(x.childNodes[1].dataset.categoryname == categSquare.dataset.categ){
-                x.classList.add('active');
-            }
-        });
-        
-    })
+    if(categSquare.dataset.categ!='customatta'){
+        categSquare.addEventListener('click',() => {
+            const billsec = document.querySelector('.billing-section');
+            billsec.style.display = 'none';
+            topRow.style.display = 'flex';
+            products.style.display = 'none';
+
+            displayItems(categSquare.dataset.categ);
+            shoppingarea.style.display = 'grid';
+            const products = document.querySelector('#products');
+            // console.log(categoryContainers);
+            categoryContainers.forEach((x) => {
+                x.classList.remove('active');
+                // console.log(x.childNodes[1].dataset.categoryname);
+                // console.log(categSquare.dataset.categ);
+                if(x.childNodes[1].dataset.categoryname == categSquare.dataset.categ){
+                    x.classList.add('active');
+                }
+            });
+            
+        })
+    }
+    else if(categSquare.dataset.categ=='customatta'){
+        categSquare.addEventListener('click',() => {
+            const billsec = document.querySelector('.billing-section');
+            billsec.style.display = 'none';
+            topRow.style.display = 'flex';
+            products.style.display = 'none';
+            customAttaSection = document.querySelector('.custom-atta');
+            customAttaSection.classList.remove('invisible');
+            createdAtta = document.querySelector('.created-atta');
+
+            availableAttas = document.querySelector('.available-attas');
+            // console.log(availableAttas);
+            fetch('./items.json')
+            .then( response => response.json())
+            .then(json =>{
+                for(let i=0;i<json.length;i++){
+                    if(json[i]['type'] == 'atta'){
+                        // console.log(json[i]);
+                        //item div created
+                        const item = document.createElement('div');
+                        item.className="item";
+                
+                        //item info
+                        const itemInfo = document.createElement('div');
+                        itemInfo.className = "item-info";
+                
+                        const itemName = document.createElement('h2');
+                        itemName.innerText = json[i]['name'];
+                
+                        const itemPrice = document.createElement('p');
+                        itemPrice.innerText = "₹" + json[i]['priceperkg'].toString() + "/kg";
+                
+                        itemInfo.append(itemName,itemPrice);
+                
+                        //quantity controls
+                
+                        const qtyControls = document.createElement('div');
+                        qtyControls.className = "qty-controls";
+                
+                        const reduceQtyButton = document.createElement('button');
+                        reduceQtyButton.className = "reduceqty-btn";
+                        reduceQtyButton.innerHTML="-";
+                
+                        const increaseQtyButton = document.createElement('button');
+                        increaseQtyButton.className = "increaseqty-btn";
+                        increaseQtyButton.innerHTML="+";
+                
+                        const qty = document.createElement('input');
+                        qty.type="number";
+                        qty.className = "qty";
+                        qty.value=1;
+                
+                        qtyControls.append(reduceQtyButton,qty,increaseQtyButton);
+                
+                        //unit controls
+                
+                        const kgButton = document.createElement('div');
+                        kgButton.className = "kg-unit-btn";
+                        kgButton.innerHTML="KG";
+                
+                        const gButton = document.createElement('div');
+                        gButton.className = "g-unit-btn";
+                        gButton.innerHTML="G";
+                
+                        const addButton = document.createElement("button");
+                        addButton.className = "add-item-btn";
+                        addButton.innerHTML = "ADD";
+                
+                        //functionality to increase and decrease qty 
+                        increaseQtyButton.addEventListener('click', () => {
+                            let curQty = qty.value;
+                            // console.log(curQty);
+                            curQty++;
+                            qty.value=curQty;
+                        });
+                        reduceQtyButton.addEventListener('click', () => {
+                            let curQty = qty.value;
+                            // console.log(curQty);
+                            curQty = (curQty==1) ? 1 : curQty-1;
+                            qty.value=curQty;
+                        });
+                        qty.addEventListener('input',() => {
+                            if(qty.value<1){
+                                qty.value=1;
+                                alert("Cannot be less than 1");
+                            }
+                        })
+                
+                        //functionality to unit controls
+                
+                        kgButton.classList.add("active");
+                
+                        kgButton.addEventListener('click',() => {
+                            if(!kgButton.classList.contains('active')){
+                                gButton.classList.remove('active');
+                                kgButton.classList.add('active');
+                            }
+                        })
+                        gButton.addEventListener('click',() => {
+                            if(!gButton.classList.contains('active')){
+                                kgButton.classList.remove('active');
+                                gButton.classList.add('active');
+                            }
+                        })
+                
+                        //functionality to add price button
+                        addButton.addEventListener('click',() => {
+                            
+                            // const totalamtdiv = document.querySelector('.total-amount');
+                            // totalamtdiv.innerHTML = "Total Amount: ₹" + totalAmt.toString();
+                
+                            //cart item creation
+                            const attacomponent = document.createElement('div');
+                            attacomponent.className='atta-component';
+                
+                            const AttacomponentName = document.createElement('h5');
+                            AttacomponentName.innerHTML="+ " + json[i]['name'];
+                
+                            // cart quantity div
+                            const componentQty = document.createElement('p');
+                            componentQty.className='component-qty';
+                            componentQty.innerHTML = qty.value;
+                
+                            const componentUnit = document.createElement('div');
+                            componentUnit.className='component-unit';
+                
+                            if(kgButton.classList.contains("active"))
+                                componentUnit.innerHTML='KG';
+                            else{
+                                componentUnit.innerHTML='G';
+                            }
+                            
+                            const priceAreaText = document.createElement('span');
+                            priceAreaText.innerHTML = "Price: ";
+                            const priceArea = document.createElement('p');
+                            priceArea.className = "component-price";
+                            if(kgButton.classList.contains("active"))
+                                priceArea.innerText=(qty.value * json[i]['priceperkg']).toString();
+                            else{
+                                priceArea.innerText=(qty.value * json[i]['priceperkg']/1000).toString();
+                            }
+                
+                            const deleteButton = document.createElement('button');
+                            deleteButton.innerHTML="Delete";
+                            
+                            attacomponent.append(AttacomponentName,componentQty,componentUnit,priceAreaText,priceArea,deleteButton);
+                
+                            createdAtta.append(attacomponent);
+                            
+                            const allComponents = document.querySelectorAll('.atta-component');
+                            const totalwtarea = document.querySelector('.custom-atta-weight');
+                            const totalpricearea = document.querySelector('.custom-atta-price');
+                            
+                            const updatePrice = () =>{
+                                let customamount = 0;
+                                let customwt = 0;
+                                const allofthoseComponents = document.querySelectorAll('.atta-component');
+
+                                allofthoseComponents.forEach((thatComponent) => {
+                                    // console.log(thatComponent.childNodes[4].textContent);
+                                    if(thatComponent.childNodes[2].textContent == 'KG')
+                                        customwt+=parseFloat(thatComponent.childNodes[1].textContent);
+                                    else
+                                        customwt+=parseFloat(thatComponent.childNodes[1].textContent)/1000;
+
+                                    customamount+=parseFloat(thatComponent.childNodes[4].textContent);
+
+                                    
+                                    
+                                });
+                                totalpricearea.innerHTML="Total Amount: Rs. "+customamount;
+                                totalwtarea.innerHTML = "Total Weight: " + customwt + " KG";
+                                console.log(customamount);
+
+                            };
+
+                            deleteButton.addEventListener('click',() => {
+                                attacomponent.remove();
+                                updatePrice();
+                            })
+                            updatePrice();
+                            
+                                        
+                        });
+                        //appending all to item div
+                
+                        item.append(itemInfo,qtyControls,kgButton,gButton,addButton);
+                
+                        availableAttas.append(item);
+                
+                
+                        }
+                }
+            })
+        })
+    }
 });
 
 const backToHome = document.querySelector('.back-to-home');
